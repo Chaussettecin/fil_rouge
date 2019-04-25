a2fa4db421c7ad5a3db169de4811c16cc85d19bb
Chaussettecin <lachaussette@gmail.com>
2019-04-25 14:58:13 +0200

    udpade food
    clean code

--- src/rpg_filrouge/equipement_food.java
+++ src/rpg_filrouge/equipement_food.java
@@ -1,70 +1,77 @@
 package rpg_filrouge;
 
+import java.util.ArrayList;
+
 public class equipement_food {
-
-
-	//Liste Aliments qui permet au joueur d'avoir des boost (force / dexiterité) augmente les capacitié
+	//Liste Aliments qui permet au joueur d'avoir des boost (force / dexiteritÃ©) augmente les capacitÃ©s
+	
 		public static final ArrayList<equipement_food> arrayFood = new ArrayList<>();
     	public static int totalAlimentsConso;
     	private String nom;
     	private String desc;
     	private int quantite = 1;
-    	private static joueurs_statusEffets.type effetsStats;
+    	private static perso_statusEffect.type effetsStats;
     	private int effetsNiv;
     	private type AlimentType;
     	private boolean viewedAbout;
 
     
-  public equipement_food(String nom, String desc, joueurs_statusEffets.type effetsStats,
-		  					type AlimentType, int effetsNiv) {
-        this.nom = nom;
-        this.desc = desc;
-        this.effetsStats = effetsStats;
-        this.AlimentType = AlimentType;
-        this.effetsNiv = effetsNiv;
-        arrayFood.add(this);
-   }// fin equipement aliments
+    	public equipement_food(String nom, String desc, perso_statusEffect.type effetsStats,
+		  						type AlimentType, int effetsNiv) {
+        
+    		this.nom = nom;
+    		this.desc = desc;
+    		this.effetsStats = effetsStats;
+    		this.AlimentType = AlimentType;
+    		this.effetsNiv = effetsNiv;
+    		arrayFood.add(this);
+   
+    	}// fin equipement aliments
 
     
-  public static void liste() {
-	  while (true) {
-		  ui.cls();
-		  int j = 0;
-		  int[] offset = new int[arrayFood.size()];
+    	public static void liste() {
+    		
+    		while (true) {
+    			
+    			ui.cls();
+    			int j = 0;
+    			int[] offset = new int[arrayFood.size()];
             
-		  for (int i = 0; i < arrayFood.size(); i++) {
-			  if (arrayFood.get(i).quantite > 0) {
-				  ui.println((j + 1) + ") " + arrayFood.get(i).getNom() + "(" + arrayFood.get(i).quantite + ")");
-				  offset[j] = i - j;
-				  j++;
-			  }//fin IF
-		  }//fin for
+    			for (int i = 0; i < arrayFood.size(); i++) {
+    				
+    				if (arrayFood.get(i).quantite > 0) {
+    					ui.println((j + 1) + ") " + arrayFood.get(i).getNom() + "(" + arrayFood.get(i).quantite + ")");
+    					offset[j] = i - j;
+    					j++;
+    				}//fin IF
+    			}//fin for
             
-      	ui.println((j + 1) + ") Retour");
-        while (true) {//retourner sur le menu des équipements "menuObjets"
+    			ui.println((j + 1) + ") Retour");
+    			
+    			while (true) {//retourner sur le menu des Ã©quipements "menuObjets"
    			
-        	int menuObjets = ui.getValidInt();
+    				int menuObjets = ui.getValidInt();
 
-               try { //Autres choix dispo dans le tab
+    				try { //Autres choix dispo dans le tab
                   
-            	   if (menuObjets == (j + 1) ||menuObjets > j)
-            		   return;
+    					if (menuObjets == (j + 1) ||menuObjets > j)
+    						return;
 
-            	   //Retour dans le menu objets / armes
-                    menuObjets--;
-                   	menuObjets = menuObjets + offset[menuObjets];
+    					//Retour dans le menu objets / armes
+    					menuObjets--;
+    					menuObjets = menuObjets + offset[menuObjets];
                     
-                   	// un effet de 1 sur le status est selectionne utiliser Switch pour voir si ok
-                   	if (arrayFood.get(menuObjets).geteffetsStat() ==joueurs_statusEffets.type.SANTE && joueurs_sante.get() == joueurs_sante.getOutOf()) {
-                        ui.msg("Tu es beau comme un sous neuf. Tu ne peux pas utiliser cet objet!");
-                        return;
-                   	}
+                 //--- un effet de 1 sur le status est selectionne utiliser Switch pour voir si ok -
+    					if (arrayFood.get(menuObjets).geteffetsStat() ==perso_statusEffect.type.SANTE && perso_sante.get() == perso_sante.getOutOf()) {
+    						ui.msg("Tu es beau comme un sous neuf. Tu ne peux pas utiliser cet objet!");
+    						return;
+    					}
 
-                   	//Resultats
-                   	if (arrayFood.get(menuObjets).quantite > 0) {
-                       equipement_food.arrayFood.get(menuObjets).manger();
-                   	}
-                   	return;
+                 //--- Resultats - 
+    					if (arrayFood.get(menuObjets).quantite > 0) {
+    						equipement_food.arrayFood.get(menuObjets).manger();
+    					}
+    					return;
 
                 	} catch (Exception e) {
                 		ui.println();
@@ -74,44 +81,44 @@ public class equipement_food {
       		} //fin while
     }//fin liste
 
-    
-  public String getNom() {
+//--- Getter --    
+    public String getNom() {
         return this.nom;
     }
 
 
-  public int getEffetsNiv() {
+    public int getEffetsNiv() {
         return effetsNiv;
     }
 
 
-  public joueurs_statusEffets.type geteffetsStat() {
+    public perso_statusEffect.type geteffetsStat() {
        return effetsStats;
-  }
+    }
 
     
-  public int getQuantite() {
+    public int getQuantite() {
         return quantite;
     }
 
     
-  public void setQuantite(int quantite) {
+    public void setQuantite(int quantite) {
         this.quantite = quantite;
     }
     
-  
-  public void manger() {
-      ui.cls();
+ //--- Consommation de l'event -
+    public void manger() {
+      
+    	ui.cls();
+    	ui.println("Tu as avalÃ©(e) : " + getNom());
+    	ui.println("Tu as gagnÃ©(e) " + effetsNiv + " " + effetsStats.toString() + " points.");
+      	this.quantite--;
+      	totalAlimentsConso++;
+      	ui.pause();
 
-      ui.println("Tu as avalé(e) : " + getNom());
-      ui.println("Tu as gagné(e) " + effetsNiv + " " + effetsStats.toString() + " points.");
-      this.quantite--;
-      totalAlimentsConso++;
-      ui.pause();
-
-      switch (this.effetsStats) {
+      	switch (this.effetsStats) {
             case SANTE:
-                joueurs_sante.gains(this.effetsNiv);
+                perso_sante.gains(this.effetsNiv);
                 break;
             case TAILLE:
                 break;
@@ -121,20 +128,24 @@ public class equipement_food {
 
    }//fin classe manger
 
-  public void viewPropos() {
-    final int BORDER_LENGTH = 39;
+    public void viewPropos() {
+    	
+    	final int BORDER_LENGTH = 39;
 
     	//fin des informations sur les aliments
         ui.cls();
+        
         for (int i = 0; i < BORDER_LENGTH; i++) ui.print("-");
         ui.print(" ");
+        
         for (int i = 0; i < ((BORDER_LENGTH / 2) - (this.getNom().length() / 2)); i++)
         ui.print(" ");
         ui.println(this.getNom());
         ui.println(this.desc);
-        ui.println("Catégories : " + this.AlimentType.toString());
+        ui.println("CatÃ©gories : " + this.AlimentType.toString());
         ui.println("Donnes les effets : " + equipement_food.effetsStats.toString());
         ui.println("Level: " + this.getEffetsNiv());
+        
         for (int i = 0; i < BORDER_LENGTH; i++) ui.print("-");
         ui.pause();
         ui.cls();
@@ -143,15 +154,15 @@ public class equipement_food {
   	}//fin vue a propos
 
  
-  public boolean affichagePropos() {
+    public boolean affichagePropos() {
         return this.viewedAbout;
     }
 
-  public void setViewed(boolean v) {
+    public void setViewed(boolean v) {
         this.viewedAbout = v;
     }
    
-  public enum type {
+    public enum type {
         VIANDE_POISSONS,
         VIANDE_HUMAINE,
         FRUIT_MAGIQUE,
@@ -161,7 +172,7 @@ public class equipement_food {
       public String toString() {
             String effectString = super.toString();
             effectString = effectString.replace("_", " ");
-            effectString = effectString.replace("Viande séchée", "");
+            effectString = effectString.replace("Viande sÃ©chÃ©e", "");
             effectString = effectString.trim();
             return effectString.substring(0, 1) + effectString.substring(1).toLowerCase();
       }
