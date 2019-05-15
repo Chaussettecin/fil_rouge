4ad1be9d174741bcfc83f376d87d0b602222906d
Chaussettecin <lachaussette@gmail.com>
2019-05-15 08:57:38 +0200

    Add map  and co
    Exemple de MAP 
    Setting
    
    Et save exemple de Json style

---
+++ map/Level.java
@@ -0,0 +1,22 @@
+package map;
+
+
+//-- A Level has a name and average map level with a composite key (worldIndex, levelIndex) --
+public class Level {
+
+//--- key
+    public int worldIndex;
+    public int levelIndex;
+
+    public String name;
+    public int avgLevel;
+
+    public Level(int worldIndex, int levelIndex, String name, int avgLevel) {
+       
+    	this.worldIndex = worldIndex;
+        this.levelIndex = levelIndex;
+        this.name = name;
+        this.avgLevel = avgLevel;
+    }
+
+}
