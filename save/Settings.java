50e6e98ff0fe01032617c3a55bf2c992e2d4fc95
Chaussettecin <lachaussette@gmail.com>
2019-05-15 16:48:23 +0200

    update setting
    voir si on met des reglages du joueurs

--- save/Settings.java
+++ save/Settings.java
@@ -1,23 +1,15 @@
-package com.unlucky.save;
+package save;
 
 /**
  * Stores the settings of the game including volume of music
- * and sfx, animation toggles, etc.
- *
- * @author Ming Li
+ * and sfx, animation toggles, etc
  */
-public class Settings {
-
-    // volume
-    // values between 0.f and 1.f (min and max)
-    public float musicVolume = 1.f;
-    public float sfxVolume = 1.f;
-    public boolean muteMusic = false;
-    public boolean muteSfx = false;
+pub
 
     // toggles
     public boolean showEnemyLevels = false;
     public boolean showWeatherAnimations = true;
     public boolean showFps = false;
+
 
 }
