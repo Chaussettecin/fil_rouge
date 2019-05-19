877a6b2de902894228cf3788b22e15a1a98c9190
Chaussettecin <lachaussette@gmail.com>
2019-05-19 17:04:19 +0200

    Update Armes Melees

--- inventory/armes_melees.java
+++ inventory/armes_melees.java
@@ -1,29 +1,82 @@
 package inventory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import Battle.battleAttaque;
 import perso.perso;
+import resource.utilsGame;
+
 
 public abstract class armes_melees extends battleAttaque{
 	
-private int minDext, minTaille;
+//-- Accuracie
+	private int minDext;
 
-	public armes_melees(String tempNom, String tempDescription, boolean tempPlural, int tempDes, 
-		int tempAdd, int tempMinTaille, int tempMinDext, int tempPrix, boolean tempTwoHands) {
-		super(tempNom, tempDescription, tempPlural, tempDes, tempAdd, tempPrix, tempTwoHands);
-		minTaille = tempMinTaille;
-		minDext = tempMinDext;
+//--Constructor - 
+	public armes_melees(String tempNom, String tempDescription, boolean tempPlural, 
+						int tempDes, int tempAdd,int tempMinDext,
+						int tempPrix, boolean tempTwoHands) {
+		
+				super(tempNom, tempDescription, tempPlural, tempDes, tempAdd, 
+						tempPrix, tempTwoHands);
+		
+				minDext = tempMinDext;
 	}
 	
-	public int getMinTaille() {
-		return minTaille;
+//-- Liste armes melees 
+	public static ArrayList<armes_melees> getAll(){ 
+		
+		return new ArrayList<armes_melees>(
+				
+				Arrays.asList(new armes_melees[]{
+						dirk(utilsGame.playerAdjectives()), 
+						epeeCourte(utilsGame.playerAdjectives()),		
+						grall(utilsGame.playerAdjectives()),
+						epeeMagique(utilsGame.playerAdjectives()),
+						baton(utilsGame.playerAdjectives()),
+			
+				}));
 	}
+		 
 	
+	public static armes_melees dirk(String description) {
+		return new armes_melees();
+	}
+		 
+	public static armes_melees grall(String description) {
+		return new armes_melees();
+	}
+		 
+	public static armes_melees epeeMagique(String description) {
+		return new armes_melees("Epée Magique", description, 
+		false, 6, 0, 21, 18, 240, true);
+	}
+		 
+ 
+	public static armes_melees epeeCourte(String description) {
+		return new armes_melees("épée courte", description, 
+		false, 3, 0, 7, 3, 35, false);
+	}
+		 
+	public static armes_melees axe(String description) {
+		return new armes_melees("axe", description, 
+		false, 4, 0, 17, 8, 100, false);
+	}
+		 
+	public static armes_melees baton(String description) {
+		return new armes_melees("baton", description, 
+				false, 2, 0, 3, 2, 6, false);
+	}
+		 
+
 	public int getMinDext() {
 		return minDext;
 	}
+// Methode pour voir si le perso possede deja ou peut utiliser l'arme
 	
-	public boolean peutUtiliser(perso Perso) {
-		return (Perso.getTaille() >= minTaille && Perso.getDexterite() >= minDext);
-	}
+	//public boolean peutUtiliser(perso Perso) {
+		//return (Perso.equips  = Perso.setAccuracy(minDext);
+	//}
 	
 }
