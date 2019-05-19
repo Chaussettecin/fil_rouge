defdfaebf1f4a1f1770f053da4eef170f49ea0a9
Chaussettecin <lachaussette@gmail.com>
2019-05-19 16:58:17 +0200

    update Equipement

--- inventory/Equipment.java
+++ inventory/Equipment.java
@@ -1,42 +1,20 @@
 package inventory;
 
-//-- La collection d'équipes que le joueur a équipée -
-
+//-- La collection d'Ã©quipes que le joueur a Ã©quipÃ©e -
 
 public class Equipment {
 
- //--- equipement :
-     //0 - helmet
-     //1 - armor
-     //2 - weapon
-     //3 - gloves
-     //4 - shoes
-     //5 - necklace
-     //6 - shield
-     
-  
-    public static final int NUM_SLOTS = 8;
-
+    public static final int NUM_SLOTS = 4;
     public Item[] equips;
     
-    //public Vector2[] positions;
+    //-- Arme dispo pour le joueur, 2 armes par joueurs
+    //max 2 Ã¹aisn pour manier l'arme
+    private int nbrMain = 2 ;
+    
+    public Equipment() {	
+	}
 
-   /* public Equipment() {
-    	//-- stock les positions des machines à sous par rapport aux stocks
-        equips = new Item[NUM_SLOTS];
-        positions = new Vector2[NUM_SLOTS];
-
-        positions[0] = new Vector2(42, 42);
-        positions[1] = new Vector2(42, 26);
-        positions[2] = new Vector2(26, 26);
-        positions[3] = new Vector2(58, 26);
-        positions[4] = new Vector2(42, 10);
-        positions[5] = new Vector2(10, 42);
-        positions[6] = new Vector2(10, 26);
-        positions[7] = new Vector2(10, 10);
-    }*/
-
-//-- Le joueur équipé d'un objet qui est placé dans le bon emplacement
+	//-- Le joueur Ã©quipÃ© d'un objet qui est placÃ© dans le bon emplacement
     //--Retourne false si impossible
     public boolean addEquip(Item equip) {
         if (equips[equip.type - 2] == null) {
@@ -47,7 +25,7 @@ public class Equipment {
         return false;
     }
 
-//-- Supprime une équipe à un index et renvoie l'élément
+//-- Supprime une Ã©quipe Ã  un index et renvoie l'Ã©lÃ©ment
     public Item removeEquip(int index) {
         Item ret = null;
         if (equips[index] != null) {
@@ -58,9 +36,18 @@ public class Equipment {
         return null;
     }
 
-//--- Retourne l'équipement d'un index spécifique mais ne le supprime pas
+//--- Retourne l'Ã©quipement d'un index spÃ©cifique mais ne le supprime pas
     public Item getEquipAt(int index) {
         return equips[index];
     }
 
+    // Getter -----------------------------
+    public int getNbrMain() {
+        return nbrMain;
+    }
+
+    // Setter -----------------------------
+    public void setNbrMain(int nbrMain) {
+        this.nbrMain = nbrMain;
+    }
 }
