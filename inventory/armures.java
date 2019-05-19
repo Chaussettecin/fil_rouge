d24f62bb6ef1fc795a18decb17468e74bd0987ef
Chaussettecin <lachaussette@gmail.com>
2019-05-19 17:00:47 +0200

    update Armure

--- inventory/armures.java
+++ inventory/armures.java
@@ -1,18 +1,33 @@
 package inventory;
 
-import Battle.battleDefence;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class armures  extends battleDefence{
+
+import Battle.battleNode;
+
+public class armures  extends Equipment{
+	
+	public String descriptionArmures;
+	public String nomArmure;
 	 
 	public armures(String tempNom, String tempDescription, int tempMinDex, int tempMinStr, 
 			int tempDefenseValue, int tempPrix, int tempDexDebuff, int tempSpeedDebuff, 
 			int tempMagicResistance) {
 		
-			super(tempNom, tempDescription, tempMinDex, tempMinStr,  tempDefenseValue, 
-			tempPrix, tempDexDebuff,  tempSpeedDebuff, tempMagicResistance);
+			super();
 	}
 
+	//-- Liste Artmures -- 
+		public static ArrayList<armures> getAll() {
+			return (new ArrayList<armures>(Arrays.asList(new armures[]{cuire("normale")})));
+		}
+    
+		public static armures cuire(String discription) {
+			return new armures("cuire", discription, 5, 5, 10, 50, 1, 1, 0);
+		}
+		
 	public String toString(){
-		return ("Equiper de " + description + " " + nom + " armures.");
+		return ("Equiper de " + descriptionArmures + " " + nomArmure + " armure.");
 	}
 }
