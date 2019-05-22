0b4544af043dd489e7f8f700dcee6467c1a6a6b3
Chaussettecin <lachaussette@gmail.com>
2019-05-22 16:35:05 +0200

    update Perso XP

--- perso/persoXP.java
+++ perso/persoXP.java
@@ -3,51 +3,54 @@ package perso;
 import javax.swing.JOptionPane;
 
 public class persoXP extends perso {
+	
+	//Variables
+			private static int xp;
+			private static int combatXP;
+			private static int xpManquant;
+			private static int level;
+			public static int totale;
 
 	public persoXP(String id, String nom, String description, int level, Race race, int ptv, int ptvMin, boolean dead,
-			int minDamage, int maxDamage, int gold, int attaque, int bonusAttaque, int defense, int accuracy,
-			int bonusDefense, int xp, int maxXP, int previousXP, boolean presenceArmure, boolean mainslivre) {
+					int minDamage, int maxDamage, int gold, int attaque, int bonusAttaque, int defense, int accuracy,
+					int bonusDefense, int xp, int maxXP, int previousXP, boolean presenceArmure, boolean mainslivre) {
 		
-		super(id, nom, description, level, race, ptv, ptvMin, dead, minDamage, maxDamage, gold, attaque, bonusAttaque,
-				defense, accuracy, bonusDefense, presenceArmure, mainslivre);
+		
+		super(id, nom, description, level, ptv, ptvMin, dead, minDamage, maxDamage, defense, accuracy);
 		
 	}
 
-	//Variables
-		private static int xp;
-		private static int combatXP;
-		private static int xpManquant;
-		private static int level;
-		public static int totale;
-
-	//--- Level up // 
-		private static void levelUp() {
-		    if (level == 9) {
-		        ui.popup("Vous avez gagné un niveau !\n Tu as gagné 250 Golds!", "Level Up !",JOptionPane.INFORMATION_MESSAGE);
+	
+//--- Level up --- 
+	private static void levelUp() {
+		    
+		if (level == 9) {
+		        System.out.printf("Vous avez gagnÃ© un niveau !\n Tu as gagnÃ© 250 Golds!", "Level Up !");
 		        level = 10;
-		        gold.set(250, true);
+		        Gold.gold.set(250, true);
 
-			} else if(level > 100){
-				game_support.error("Erreur- Les niveau de ce jeu ne dépasse pas 100");
+		} else if(level > 100){
+				System.out.printf("Erreur- Les niveau de ce jeu ne dÃ©passe pas 100");
 		        level = 100;
 		        
-			} else if(level == 99){
+		} else if(level == 99){
 		        level = 100;
 		        xp = 49000;
 		        
-			}else {
-				ui.popup("Level Up ! Maintenant tu es" + (level + 1) + "!\n Tu remportes 100 Golds!","Level Up !" ,JOptionPane.INFORMATION_MESSAGE);
+		}else {
+				System.out.printf("Level Up ! Maintenant tu es" + (level + 1) + "!\n Tu remportes 100 Golds!","Level Up !");
 				xp = 0;
 				xpManquant += 500;
 				level++;
-				gold.set(100, true);
-			}//Fin if
+				Gold.gold.set(100, true);
+		}//Fin if
 		        
 			//quetes.check();
-		 }//fin levelUp()
+	}//fin levelUp()
 
 			
-		public static void set(int montant, boolean add) {
+		
+	public static void setXP(int montant, boolean add) {
 		     
 		     if (add){
 		         xp += montant;
@@ -59,66 +62,65 @@ public class persoXP extends perso {
 			if ((xp >= xpManquant) && (xpManquant != 0)){
 				int xpRestant = xp - xpManquant;
 		        levelUp();
-		        set(xpRestant, true);
+		        setXP(xpRestant, true);
 			}
-		 }//fin set
 			
-		public static int getCombatXp() {
+	}//fin set
+			
+		
+	public static int getCombatXp() {
 			return combatXP;
-		}
+	}
 			
-		public static void setCombatXp(int montant, boolean add) {
+		
+	public static void setCombatXp(int montant, boolean add) {
 				
-			if(add) {
+		if(add) {
 				combatXP += montant;
-			} else {
+		} else {
 				combatXP = montant;
-			}
-		} //fin setCombatXP
+		}
+	} //fin setCombatXP
 
-		public static void setLevel(int lvl){
+		
+	public void setLevel(int lvl){
 			level = lvl;
-		}
+	}
 			
-		public static void setOutOf(int outOf){
+		
+	public static void setOutOf(int outOf){
 			xpManquant = outOf;
-		}
+	}
 			
-		public static void setAll(int actuel, int outOf, int lvl){
+		
+	public static void setAll(int actuel, int outOf, int lvl){
 		    xp = actuel;
 		    xpManquant = outOf;
 		    level = lvl;
-		}
+	}
 
-		public static int get(){
+		
+	public static int getXP(){
 			return xp;
-		}
+	}
 
-		public static int getOutOf(){
+		
+	public static int getOutOf(){
 			return xpManquant;
-		}
+	}
 
-		public static int getLevel(){
+		
+	public int getLevel(){
 			return level;
-		}
+	}
 
-		public static String getFull(){
+		
+	public static String getFull(){
 			return xp + "/" + xpManquant + " xp";
-		}
+	}
 
-		public static int svUtiliser = 0;
-		public static int svNiveau;
-		public static int svPrix;
-		 //-- ajoute des potion pour soigner les statuts 
-		 //-- ajouter une potion qui donne au perso un coup de pouce temp (avec + de dégâts)
-
-		public static int 	restoUtiliser = 0;
-		public static int 	restNiveau;
-		public static int 	restPrix;
-		private static int 	potionSuvie; //potion + 25% sante
-		private static int 	potionRestau; //potion + 75% santé
-			
-		public static int get(String trouver) {
+		
+	public static int get(String trouver) {
 		     
 			switch (trouver.toLowerCase()) {
 		        case "Survie":
@@ -131,7 +133,8 @@ public class persoXP extends perso {
 
 		} // fin getTrouver
 
-		public static void set(String trouver,int montant, boolean add) {
+		
+	public static void set(String trouver,int montant, boolean add) {
 		     
 			switch (trouver.toLowerCase()) {
 		        case "survie":
@@ -170,10 +173,10 @@ public class persoXP extends perso {
 
 		     } else if (perso_sante.get() == 100) {
 		    	 ui.println("-------------------------------------------------------");
-		         ui.println("Tu es déjà en pleine forme ! Tu n'est pas un chat");
+		         ui.println("Tu es dÃ©jÃ  en pleine forme ! Tu n'est pas un chat");
 		         ui.println("Donc tu n'as pas besoin d'utiliser " + trouver + " potion!");
 		         ui.println("-------------------------------------------------------");
-		         ui.println("Ta santé actuelle :  " + perso_sante.getStr());
+		         ui.println("Ta santÃ© actuelle :  " + perso_sante.getStr());
 		         ui.println(trouver + "Potions : " + get(trouver));
 		         ui.println("-------------------------------------------------------");
 		         ui.pause();
@@ -186,9 +189,9 @@ public class persoXP extends perso {
 
 		         ui.println("----------------------------------------------------");
 		         ui.println("Tu viens de consommer : " + trouver + " potion.");
-		         ui.println("Te voilà en pleine forme avec : " + soin + " sante.	");
+		         ui.println("Te voilÃ  en pleine forme avec : " + soin + " sante.	");
 		         ui.println("----------------------------------------------------");
-		         ui.println("Ta santé est de: " + perso_sante.getStr());
+		         ui.println("Ta santÃ© est de: " + perso_sante.getStr());
 		         ui.println(trouver + " Potions: " + get(trouver));
 		         ui.println("----------------------------------------------------");
 		         ui.pause();
@@ -226,14 +229,14 @@ public class persoXP extends perso {
 		      int prix = getPrix(trouver);
 
 		     if (perso_xp.getLevel() < level) {
-		         ui.println("Tu n'est pas encore prêt pour ça :" + level + " pour acheter ça!");
+		         ui.println("Tu n'est pas encore prÃªt pour Ã§a :" + level + " pour acheter Ã§a!");
 		         ui.pause();
 		     
 		     } else if (prix <= gold.get()) {
 		    	 gold.set(-prix, true);
 		         perso_stats.totalGoldepenserprSante += prix;
 		         set(trouver, 1, true);
-		         ui.println("Merci pour ton achat. A bientôt! ");
+		         ui.println("Merci pour ton achat. A bientÃ´t! ");
 		         ui.pause();
 		     
 		     } else {
