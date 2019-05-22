6617f5a885de48ac950f9a39ed4f69d2f9fca3f8
Chaussettecin <lachaussette@gmail.com>
2019-05-22 16:29:08 +0200

    update Race

--- perso/Race.java
+++ perso/Race.java
@@ -2,17 +2,26 @@ package perso;
 
 public interface Race {
 
-	// retourne le bonus d'attaque de race
+/--Differentes classe de perso -
+    public enum ClassesPerso{
+
+        ELFE,
+        ORC,
+        Humain,
+        }
+    
+
+	/*// retourne le bonus d'attaque de race
 	    public int BonusAttaque(perso personnage,perso enemi);
 
 	    public boolean parer(perso personnage,perso enemi);
 
-	 // methode les effets, bonus ou malus du pouvoir définit par la race choisie
+	 // methode les effets, bonus ou malus du pouvoir dÃ©finit par la race choisie
 	    public boolean pouvoir(perso personnage);
 	   
 	    public String toString();
 
 	    public boolean isUtilisationPouvoir();
 
-	    public void setUtilisationPouvoir(boolean utilisationPouvoir);
+	    public void setUtilisationPouvoir(boolean utilisationPouvoir);*/
 }
