dd9437aa5d158e8a34ee0a344026cb7185e91732
Chaussettecin <lachaussette@gmail.com>
2019-06-06 16:42:18 +0200

    update Ennemis

--- enemy.java
+++ enemy.java
@@ -2,14 +2,12 @@ package enemys;
 
 import java.util.Random;
 
-import Battle.FaintedAction;
 import Battle.battleAction;
 import Effect.statusEffect;
 import inventory.armes_sorts;
 import perso.Race;
 import perso.perso;
 import enemys.enemy;
-
 
 public abstract class enemy {
 	
@@ -18,14 +16,13 @@ public abstract class enemy {
     
     protected static Race race ;
 	protected static int ptv ;// ------------------pts de vie de base
-    protected static boolean dead = false; //----Verif s'il est est vivant ou pas
+    public static boolean dead = false; //----Verif s'il est est vivant ou pas
 	
 //--- Degats
+    protected static int level;
     protected static int minDamage;
     protected static int maxDamage;
 	
-    protected static int  level;
-  
     protected static int accuracy;
     protected static int attaque ;     
   	protected static int bonusAttaque ; //---------addition des differents bonus d'equipement
@@ -35,7 +32,7 @@ public abstract class enemy {
     protected static int XP;
     protected static int maxXP;
     protected static int previousXP;
-    protected static int degat= 0;
+    public static int degat= 0;
     protected static int soin = 0;
 	
 //---Battle / Status Effect -
@@ -45,9 +42,10 @@ public abstract class enemy {
 	private static final Random rand = new Random();
 
 //-- Constructor -
-    public enemy(String nom, int level,Race race,int ptv, boolean dead, int attaque, int bonusAttaque,int defense,
-    			int bonusDefense, int XP, int maxXP, int previousXP, int degat, int soin,
-    			int accuracy,  int minDamage,int maxDamage, statusEffect statusEffects) {
+    public enemy(String nom, int level,Race race,int ptv, boolean dead, int attaque,
+    			int bonusAttaque,int defense,int bonusDefense, int XP, int maxXP,
+    			  int previousXP, int degat, int soin, int accuracy,  int minDamage,int maxDamage,
+    			 statusEffect statusEffects) {
     	
     		this.nom = nom;
     		this.level = 1;
@@ -63,25 +61,25 @@ public abstract class enemy {
 
 
 //--- Battle  -- 
-  	public void attaqueBase( String nom){
+  	public void attaqueBase( perso perso){
   		
   		int variance = ((int) (Math.random() * 10)) - 5;
   		int attaqueDegats = (attaque * 2) + (accuracy * 2);
   		
   		attaqueDegats+= variance;
   		
-  		System.out.println(nom + " frappe " + nom + "pour "
+  		System.out.println(perso + " frappe " + perso + "pour "
   				+ attaqueDegats + " dégats.");
 		return;
   	}
   	
-//--- Tour de combat  	
+  	
   	protected void takeTurn(battleAction chosenAction){
         
   		if(ptv > 0){
             setBattleAction(chosenAction);              
   		}
-        performAction();
+       
     }
   	
   	
@@ -89,10 +87,10 @@ public abstract class enemy {
         BattleAction = chosenAction;
     }
 
-	
-  	private void performAction(){
-        battleAction.execute(null, enemyBoss());
-    };
+
+  	/*private void performAction(){
+        battleAction.baston(null, enemyBoss());
+    };*/
 
 //-- Sante dans le combat --
     public void santePerdu(int amount){
@@ -103,7 +101,6 @@ public abstract class enemy {
          }
     }
 
-     
     protected void santeGagner(int amount){
          
     	if(ptv + amount> 0){
@@ -114,7 +111,6 @@ public abstract class enemy {
     	
     	}
      }       
-
 
 //--- Gere les sors utilisés par l'adversaire pendant le combat
   	public void utiliseSort(perso Perso, armes_sorts Sort){
@@ -128,7 +124,8 @@ public abstract class enemy {
   
 //--- Méthode Ennemi random. 
   	 public void newRandomEnemy() {
-         int random = rand.nextInt(4) + 1;
+         
+  		 int random = rand.nextInt(4) + 1;
          
          enemy Enemy = null;
         
@@ -142,7 +139,6 @@ public abstract class enemy {
          }
      }
   	
-  	 
   	 protected enemy enemyNormal() {
   		 return enemyNormal();
   	 }
@@ -159,10 +155,8 @@ public abstract class enemy {
 	
   	 public static void setActuelleSante(int santeActuelle){ enemy.ptv= santeActuelle; }
 	
-
-  	 public static int getActuelleSante() {
-  		 return ptv;
-  	 }
+  	 public static int getActuelleSante() {return ptv; }
+  		 
   	 
 //--Définit et met à l'échelle les statistiques de l'ennemi en fonction de son type 
     //-- et de son niveau
@@ -181,19 +175,19 @@ public abstract class enemy {
 		return 0;
 	}
 
-	public static int getBonusDefense() {
-		return bonusDefense;
-	}
-
+	public static int getBonusDefense() {return bonusDefense; }
+		
 	public static void setBonusDefense(int bonusDefense) {
 		enemy.bonusDefense = bonusDefense;
 	}
    
-	public String getNom() {
-		return nom;
-	}
+	public String getNom(String text) { return nom; }
+		
 
-	
+	public int attaque() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 
 }
