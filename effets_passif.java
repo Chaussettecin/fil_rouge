0b032608c6a0f013e05614bd4e790514bc0af30f
Chaussettecin <lachaussette@gmail.com>
2019-06-15 21:08:06 +0200

    update Effect
    A remettre en place dans la partie combat

---
+++ effets_passif.java
@@ -0,0 +1,21 @@
+package Effect;
+
+
+public class effets_passif extends effect{
+	
+	public effets_passif (String tempNom,int tempStrMod, int tempdextMode, 
+							int tempmageMode, int tempintelMode, int tempconstrucMode,
+							int tempvitesseMode, int tempcharisMode, int tempchanceMode) {
+		
+		
+				super( 	tempNom,  
+						tempStrMod,  
+						tempdextMode,  
+						tempmageMode,  
+						tempintelMode,  
+						tempconstrucMode, 
+						tempvitesseMode,  
+						tempcharisMode,  
+						tempchanceMode);
+		}
+}
