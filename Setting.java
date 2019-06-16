d59f5e2ad065c989ac061f8a641db478b984cb5b
Chaussettecin <lachaussette@gmail.com>
2019-06-16 20:50:26 +0200

    Update Utils
    Setting => reglages du jeu / A voir
    HelpGame => Page d'aide de jeu pour l'utilisateur ?

---
+++ Setting.java
@@ -0,0 +1,27 @@
+package Utils;
+
+import Gold.Gold;
+import Perso.Perso;
+
+public class Setting {
+	
+
+
+//--- Reglage par defaut  d'une partie nouvelle partie  -- 
+	/*private static void nouvPartieReg() {
+
+		//-- Les perso peuvent commencer avec 50 golds des le depart
+				gold.set(50, false);
+
+		//--- Les perso ont chacun deux potions
+				potions.set("Survie", 2, false);
+				potions.set("Restauration", 2, false);
+				perso.setAll(0, 500, 1);
+
+		//-- Equipement perso -- 
+				perso.sans.setPossede(true);
+				perso.sans.equipSilent();
+			 }// fin reglage nouvelle partie 
+
+*/
+}
