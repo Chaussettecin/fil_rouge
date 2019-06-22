e6c086760cb2a1b8187336a5416450f8bead2b1b
Chaussettecin <lachaussette@gmail.com>
2019-06-22 13:41:16 +0200

    Update Orc

--- src/Perso/Orc.java
+++ src/Perso/Orc.java
@@ -1,40 +1,35 @@
 package Perso;
 
-import java.util.List;
 
-import Effect.statusEffect;
-import Gold.Gold;
-import Inventory.Item;
 
 public abstract class Orc extends Perso{
 	
 	private String description;
 	
-	public Orc(String nom, Race ClassesPerso, PersoStats Stats, PersoItems Items, 
-			int Level, int XP, int Gold, int ptv, int ptvMin, int minDegat, 
-			int maxDegat,int degat, int attaque, int bonusAttaque, int defense,
-			int accuracy,int bonusDefense, boolean presenceArmure, 
-			boolean mainslibre, statusEffect statusEffects, 
-			String description) {
+// Fes Forelash -- 
+	
+	 public Orc(String nom, Race ClassesPerso) {
+			super(nom,ClassesPerso);
 			
+			//-- A faire en fonction de nos personnage 
+		}
 		
-		super (nom, ClassesPerso, Stats, Items, Level, XP, Gold, ptv, ptvMin, 
-				minDegat, maxDegat, degat, attaque, bonusAttaque, defense, 
-				accuracy,bonusDefense, presenceArmure, mainslibre, statusEffects);
-				  
-				 
+		public Orc(String description  ) {
 			
+			super (	Nom, ClassesPerso, Stats, Items, Level, XP, 
+					Gold, ptv, ptvMin, minDegat, maxDegat, degat, 
+					attaque, bonusAttaque, defense, accuracy, 
+					bonusDefense, presenceArmure, mainlibre , 
+					statusEffects);
+			
+			//-- A faire en fonction de nos personnage 
+		}
+
 		
-		//-- A faire en fonction de nos personnage 
-	
-	
-	}
 
-	
-
-	public void descriptionFes(String dsc) {
-		dsc= "Ex cultiste d’Azaroth, il fut banni de Archityran (terre des ORcs) \n parce que la rationalité lui ai tombé sur  la tête"
-             + "Pour lui les Dieux n'existent plus." + "Il prefere la baston pour sauver l'humanité";
+	public String descriptionFes()  {
+		return "Ex cultiste dâ€™Azaroth, il fut banni de Archityran (terre des ORcs) \n parce que la rationalitÃ© lui ai tombÃ© sur  la tÃªte"
+             + "Pour lui les Dieux n'existent plus." + "Il prefere la baston pour sauver l'humanitÃ©";
 	}
 	
 //-- Elle "stringuise" ttes les valeurs pr pouvoir les afficher
@@ -43,9 +38,17 @@ public abstract class Orc extends Perso{
 		return " Nom :" + this.getNom() +
            " Point de vie :" + this.getPtv() +
            " Level :" + this.getLevel() +
-           " Gold:" + this.getMoney();
-		
-		this.descriptionFes(description);
+           " Gold:" + this.getMoney() +
+           " Description:" + this.descriptionFes();
+			
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}	
 
 }
