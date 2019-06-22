ec7f9a08f3625a803bca1d5786aca11c742070c6
Chaussettecin <lachaussette@gmail.com>
2019-06-22 13:40:47 +0200

    Unpdate Nain

--- src/Perso/Nain.java
+++ src/Perso/Nain.java
@@ -1,33 +1,27 @@
 package Perso;
 
-import Effect.statusEffect;
 
 public abstract class Nain extends Perso {
 	
 	private String description;
 
-	public Nain(String nom, Race ClassesPerso, PersoStats Stats, PersoItems Items, 
-			int Level, int XP, int Gold, int ptv, int ptvMin, int minDegat, 
-			int maxDegat,int degat, int attaque, int bonusAttaque, int defense,
-			int accuracy,int bonusDefense, boolean presenceArmure, 
-			boolean mainslibre, statusEffect statusEffects, 
-			String description) {
+	public Nain( String description) {
 		
 		
-		super(nom, ClassesPerso, Stats, Items, Level, XP, Gold, ptv, ptvMin, minDegat, maxDegat, degat, attaque, bonusAttaque,
-			defense, accuracy, bonusDefense, presenceArmure, mainslibre, statusEffects);
-		
-		
-		
+		super(Nom, ClassesPerso, Stats, Items, Level, XP, 
+				Gold, ptv, ptvMin, minDegat, maxDegat, degat, 
+				attaque, bonusAttaque, defense, accuracy, 
+				bonusDefense, presenceArmure, mainlibre , 
+				statusEffects);
 	
 	
 	}
 
 
-	public void descriptionJissi(String dsc) {
-		dsc = "Il faut toujours se méfier des petites tailles" +
+	public String descriptionJissi() {
+		return "Il faut toujours se mÃ©fier des petites tailles" +
 			"Suffit de voir son cahier de chasse, elle est la plus grande Assassin du monde."
-			+ "Son nom est sur toute les lèvres dans les tavernes.";
+			+ "Son nom est sur toute les lÃ¨vres dans les tavernes.";
 	}
 
 
@@ -37,9 +31,19 @@ public abstract class Nain extends Perso {
 		return " Nom :" + this.getNom() +
            " Point de vie :" + this.getPtv() +
            " Level :" + this.getLevel() +
-           " Gold:" + this.getMoney();
+           " Gold:" + this.getMoney() +
+           " Description:" + this.descriptionJissi();
 		
-		this.descriptionJissi(description);
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
 	}	
 
 }
