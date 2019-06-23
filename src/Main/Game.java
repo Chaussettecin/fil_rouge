308eebdf530deba3e8a66928d20c7bc27d48b6f2
Chaussettecin <lachaussette@gmail.com>
2019-06-23 15:08:55 +0200

    Udapte Game.Main

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,51 +1,33 @@
 package Main;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Random;
-import java.util.Scanner;
 
 import javax.swing.JFrame;
-
-import Inventory.Inventory;
-import Menu.MainMenu;
-import Menu.Menu;
-import Perso.Perso;
 import Save.SaveSlot;
 import Main.Game;
 import UI.StartScreen;
 import UI.Screen;
-import UI.UI;
-import Utils.HelpGame;
-import Utils.Setting;
+
 import asciiPanel.AsciiPanel;
 
 public class Game extends JFrame  implements KeyListener {
 
-//public static Scanner USERINPUT = new Scanner(System.in);
-//public static final Random RAND = new Random(); 
-//public static Perso currPlayer;
-	  
 	private AsciiPanel terminal;
 	private Screen screen;
-		
+	
+//--- Main -- 	
 	public static void main(String[] args) throws IOException {
+		
 		Game app = new Game();
 		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       		 app.setVisible(true);
-	 }
+        app.setVisible(true);
 	 
-	/* private static void displayPDF(String chemin) throws IOException {
-	        Files.newDirectoryStream(Paths.get(chemin),
-	                path -> path.toString().endsWith(".*"))
-	                .forEach(System.out::println);
-	    }*/
-	
+	}
+	 
+//-- Méthode Game -- 
 	public Game() {
 		super();
 		terminal = new AsciiPanel();
@@ -53,8 +35,7 @@ public class Game extends JFrame  implements KeyListener {
 		pack();
 		screen = new StartScreen();
 		addKeyListener(this);
-		repaint();
-//		Screen sc = new StartScreen(); 
+		repaint(); 
 	}
 
 	public void repaint() {
@@ -62,8 +43,6 @@ public class Game extends JFrame  implements KeyListener {
 		screen.displayOutput(terminal);
 		super.repaint();
 	}
-	
-	
 	
 	
 	@Override
@@ -79,8 +58,12 @@ public class Game extends JFrame  implements KeyListener {
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {}
-
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+	
 //--- Démarer la partie à partir d'une sauvegarde existente -- 	    
 	public static void loadGame() {
 	   SaveSlot.load();
@@ -89,16 +72,10 @@ public class Game extends JFrame  implements KeyListener {
 
 //-- Aide de jeu  --- 
 	public static void helpGame() {
-	   // HelpGame.menu();
+	   // HelpGame.menu(); /// Faire une partie aide pour le joueur ?
 	}
 
-//-- Reglage ---
-	public static void Settings() {
-		Settings();
-	}
-
-
-	public void actionPerformed(ActionEvent e) {	}
-
+	public void actionPerformed(ActionEvent e) {}
+		
 
 }
