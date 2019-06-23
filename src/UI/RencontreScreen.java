4ed060e65aeb76b2a39e63674d013446ae01207b
Chaussettecin <lachaussette@gmail.com>
2019-06-23 21:53:53 +0200

    Update and rename rencontreScreen.java to RencontreScreen.java

--- src/UI/RencontreScreen.java
+++ src/UI/RencontreScreen.java
@@ -2,38 +2,49 @@ package UI;
 
 import java.awt.event.KeyEvent;
 
+import Enemy.Enemy;
 import asciiPanel.AsciiPanel;
 import Serialization.Serialiser;
 
-public class rencontreScreen implements Screen {
+
+/*
+ * Screen interaction 
+ */
+
+public class RencontreScreen implements Screen {
 	
 	PlayScreen screenBefore;
 	
-	public rencontreScreen(PlayScreen before, int ennemie) {
+	public RencontreScreen(PlayScreen before,int enn) {
 		
 		this.screenBefore=before;
-		screenBefore.map.map[screenBefore.bot.get(ennemie).position.dy][screenBefore.bot.get(ennemie).position.dx]=' ';;
-		screenBefore.bot.remove(ennemie);
+		screenBefore.map.map[screenBefore.bot.get(enn).position.dy][screenBefore.bot.get(enn).position.dx]=' ';;
+		screenBefore.bot.remove(enn);
 	
 	}
-	
 	
 	@Override
 	public Screen respondToUserInput(KeyEvent key) {
+		
 		if(key.getKeyCode()==KeyEvent.VK_ENTER) {
-			
 			return screenBefore;
-		}else if (key.getKeyChar()=='s') {
+		
+		} else if (key.getKeyChar()=='s') {
 			new Serialiser(screenBefore,"testSav.json");
-		}	
+		}
+		
 		return this;
 	}
+
+//--Interaction avec un perso de la map ...  
+	//voir classe PlayScreen 
 
 	@Override
 	public void displayOutput(AsciiPanel terminal) {
 		terminal.writeCenter("INTERACTION AVEC ", 14);
-		//terminal.writeCenter("La sauvegarde ------- s ------" , 18);
-
+		terminal.writeCenter("Vous avez rencontrer, Que voulez vous faire", 5);
+		
+	
 	}
 
 }
