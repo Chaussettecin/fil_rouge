0c782ed292fa41a4d1c8962ac9e1ff632f87a041
Chaussettecin <lachaussette@gmail.com>
2019-06-23 21:57:32 +0200

    Update SavLoaderScreen.java

--- src/UI/SavLoaderScreen.java
+++ src/UI/SavLoaderScreen.java
@@ -11,8 +11,8 @@ import Serialization.Donnees;
 
 public class SavLoaderScreen implements Screen {
 	
-	ArrayList<Donnees> list;
 	int i;
+	ArrayList<Donnees> list;
 	
 	public Screen respondToUserInput(KeyEvent key) {
 		
@@ -29,41 +29,42 @@ public class SavLoaderScreen implements Screen {
 						System.out.println("hello");
 						return new PlayScreen(list.get(0));
 					}	
-				
 					break;
+				
 			case KeyEvent.VK_1:
 				
 					if(i<3)
 						return new PlayScreen(list.get(1));
 					break;
+					
 			case KeyEvent.VK_2:
 				
-				if (i<4) 
-					return new PlayScreen(list.get(2));
-				
-				break;
+					if (i<4) 
+						return new PlayScreen(list.get(2));
+					break;
+			
 			case KeyEvent.VK_3:
 				
-				if(i<5)
-					return new PlayScreen(list.get(3));
-				break;
-	
+					if(i<5)
+						return new PlayScreen(list.get(3));
+					break;
 			}
-		}	
+		}
+		
 		return this;
-	
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void displayOutput(AsciiPanel terminal) {
-		
 	    	
-	    	list = new Deserialiser("sav.json").getListe();
+		list = new Deserialiser("sav.json").getListe();
 	    	
-	    	if(list.size()==0)
-	    		terminal.writeCenter("Aucune sauvegarde pour le moment", 4,Color.RED);
-	  
+	    if(list.size()==0)
+	    	
+	    terminal.writeCenter("Aucune sauvegarde pour le moment", 4,Color.RED);
 		terminal.writeCenter("Choississer votre sauvegarde : ", 2);
+		
 		for(i=0;i<list.size();i++) {
 			terminal.writeCenter(i +". "+ list.get(i).getNom(), 5+i);
 		}
