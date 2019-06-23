2581dfd10e94efaabb24c76aca8c6a7b2746f65e
Chaussettecin <lachaussette@gmail.com>
2019-06-23 22:04:30 +0200

    Update Deserialiser.java

--- src/Serialization/Deserialiser.java
+++ src/Serialization/Deserialiser.java
@@ -1,10 +1,7 @@
 package Serialization;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -12,20 +9,18 @@ import java.util.ArrayList;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import UI.PlayScreen;
-import terminalOverflow.Vector2d;
 
 public class Deserialiser{
 	   
 	private GsonBuilder builder = new GsonBuilder();
 	private Gson gson = builder.setPrettyPrinting().create();
-//	private Vector2d vect =new Vector2d();
 	private Donnees donnees;
 	private ArrayList<Donnees> al = new ArrayList<>();	
 	  
+	
 	public Deserialiser( String fichier) {
 	
-		   String json="";
+		String json="";
 		  
 		 try {
 			 
@@ -35,8 +30,9 @@ public class Deserialiser{
 			 String ligne;
 			 donnees=new Donnees();
 			 int i=0;
+			 
 			 while ((ligne=buff.readLine())!=null){
-			 	//System.out.println(i+ligne);
+
 			 	json+=ligne;
 			 	//i++;
 				donnees=gson.fromJson(ligne, Donnees.class);
@@ -51,34 +47,27 @@ public class Deserialiser{
 			 
 		 	}
 		 	
-		 	// donnees=gson.fromJson(json, Donnees.class);
 		 
-		 	 
 	   }
 	   
-	   
-	   	   
 	  
-	   
-	   public Donnees getDonnees() {
+	public Donnees getDonnees() {
+		
 		if (donnees!=null)
 			return donnees;
 		return null;
 	}
 
-
-
 	public void setDonnees(Donnees donnees) {
 		this.donnees = donnees;
 	}
 
-	public ArrayList getListe() {
+	public ArrayList<Donnees> getListe() {
 		return al;
 	}
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		   new Deserialiser("testSav.json");
-	   }
-		
-	   
+	   }*/
+		   
 }
