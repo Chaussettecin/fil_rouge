3425343d82207a33c38a8d273625514ad35cf35c
Chaussettecin <lachaussette@gmail.com>
2019-06-23 21:15:32 +0200

    Update mouvPerso

--- src/terminalOverflow/PersoMouv.java
+++ src/terminalOverflow/PersoMouv.java
@@ -1,30 +1,34 @@
 package terminalOverflow;
 
 import java.awt.Color;
+
+import Perso.Perso;
 import asciiPanel.AsciiPanel;
 import maps.Map;
 
+/**
+ * Classe de gestion des mouvements de l'équipe de personnages
+ */
 public class PersoMouv {
 	
 	private static final long serialVersionUID = 1L;
-	private String nom = "deglass";
+
+	Perso perso;
 	char tile;
-	
-	Color color;
-	//int posX;
-	//int posY;
 	int posMapX,posMapY;
-	AsciiPanel terminal;
 	Map map;
+	Color color;
+	AsciiPanel terminal;
+
 	public Vector2d position;
 	
-	
-	public PersoMouv(AsciiPanel terminal,Map map, int posX, int posY,char tile, Color color) {
+
+//-- Constructor - 
+	public PersoMouv(AsciiPanel terminal,Map map, int posX, 
+					int posY,char tile, Color color) {
 		
 		this.terminal=terminal;
-	//	this.posX=posX;
 		this.posMapX=posX;
-	//	this.posY=posY;
 		this.posMapY=posY;
 		this.tile=tile;
 		this.map=map;
@@ -33,28 +37,36 @@ public class PersoMouv {
 		
 	}
 	
-	public PersoMouv(AsciiPanel terminal,Map map, int posX, int posY,char tile, Color color, String nom) {
-		this.nom=nom;
+	public PersoMouv(AsciiPanel terminal,Map map, int posX, 
+					int posY,char tile, Color color, Perso perso) {
+		
+		this.perso = perso;
 		this.terminal=terminal;
 		this.position= new Vector2d(posX,posY);
-	//	this.posX=posX;
 		this.posMapX=posX;
-	//	this.posY=posY;
 		this.posMapY=posY;
 		this.tile=tile;
 		this.map=map;
 		this.color=color;
 	}
 	
+	
+//-- Affiche sur la map l'etat de l'equipe... PTV / Gold / Level
 	public void afficherPlayer() {
+		
 		terminal.write(tile, posMapX, posMapY, color);
 		terminal.write((char)36,terminal.getWidthInCharacters()/2,0);
 		terminal.write((char)37,terminal.getWidthInCharacters()-5,0);
+		
 		String vie="";
 		Color color = Color.WHITE;
+		
+		int ptVie = Perso.getPtv();
+		
 		if(ptVie>90) {
 			vie="||||||||||";
 			color=Color.green;
+		
 		}else if(ptVie>=80 && ptVie<90) {
 			vie="|||||||||";
 			color=Color.green;
@@ -85,9 +97,7 @@ public class PersoMouv {
 		}
 		terminal.write((char) 03,5,0,color);
 		terminal.write(vie,1,1);
-	
 	}
-	
 	
 	public void DeplacerX(int p) {
 		
@@ -102,99 +112,98 @@ public class PersoMouv {
 			posMapX+=p;
 			
 		} else {
-		
 			posMapX=terminal.getWidthInCharacters()/2-1;
 			map.setiX(map.getiX()+p);
-			
+	
 		}
-		
-		
 		
 	}
 	
 	public void DeplacerY(int p) {
+		
 		position.dy+=p;
 		
 		if ((position.dy<terminal.getHeightInCharacters()/2)) {
 			posMapY=position.dy;
 			map.setjY(0);
-		}else if(position.dy>map.hauteur-terminal.getHeightInCharacters()/2){
+		
+		} else if(position.dy>map.hauteur-terminal.getHeightInCharacters()/2){
+			
 			map.setjY(map.hauteur-terminal.getHeightInCharacters());
 			posMapY+=p;
 			
-		}
-		else {
+		} else {
+			
 			posMapY=terminal.getHeightInCharacters()/2-1;
 			map.setjY(map.getjY()+p);
 			
 		}
 	}
 
-	
-		public void dig() {
-			if(map.map[position.dy][position.dx+1]== 'X')
+	public void dig() {
+			
+		if(map.map[position.dy][position.dx+1]== 'X')
 				map.map[position.dy][position.dx+1]= ' ';
 		}
-
-		public String getNom() {
-			return nom;
-		}
-
-		public void setNom(String nom) {
-			this.nom = nom;
-		}
-
-		public char getTile() {
-			return tile;
-		}
-
-		public void setTile(char tile) {
-			this.tile = tile;
-		}
-
-		public int getPosMapX() {
-			return posMapX;
-		}
-
-		public void setPosMapX(int posMapX) {
-			this.posMapX = posMapX;
-		}
-
-		public int getPosMapY() {
-			return posMapY;
-		}
-
-		public void setPosMapY(int posMapY) {
-			this.posMapY = posMapY;
-		}
-
-		public AsciiPanel getTerminal() {
-			return terminal;
-		}
-
-		public void setTerminal(AsciiPanel terminal) {
-			this.terminal = terminal;
-		}
-
-		public Map getMap() {
-			return map;
-		}
-
-		public void setMap(Map map) {
-			this.map = map;
-		}
-
-		public Vector2d getPosition() {
-			return position;
-		}
-
-		public void setPosition(Vector2d position) {
-			this.position = position;
-		}
 		
-		
-		
-}
+	public Perso getPerso() {
+		return perso;
+	}
 
+	public void setPerso(Perso per) {
+		this.perso = per;
+	}
 
+	public char getTile() {
+		return tile;
+	}
+
+	public void setTile(char tile) {
+		this.tile = tile;
+	}
 	
+	public int getPosMapX() {
+		return posMapX;
+	}
+
+	public void setPosMapX(int posMapX) {
+		this.posMapX = posMapX;
+	}
+
+	public int getPosMapY() {
+		return posMapY;
+	}
+
+	public void setPosMapY(int posMapY) {
+		this.posMapY = posMapY;
+	}
+
+	public AsciiPanel getTerminal() {
+		return terminal;
+	}
+
+	public void setTerminal(AsciiPanel terminal) {
+		this.terminal = terminal;
+	}
+
+	public Map getMap() {
+		return map;
+	}
+
+	public void setMap(Map map) {
+		this.map = map;
+	}
+
+	public Vector2d getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector2d position) {
+		this.position = position;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+			
+}
