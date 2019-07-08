51ce0ac45e69a94203686c8d539f48e05a0f7c45
Chaussettecin <lachaussette@gmail.com>
2019-07-08 15:30:49 +0200

    Update Item

--- src/Inventory/Item.java
+++ src/Inventory/Item.java
@@ -1,51 +1,120 @@
 package Inventory;
 
+import java.awt.Color;
+import java.util.List;
+import java.util.ArrayList;
+
+import Battle.Sort;
+import Battle.Effect;
 import Inventory.ItemType;
 
-public abstract class Item {
+
+
+public class Item {
 	
-	 protected String nom;
-	 protected Integer prix;
-	 protected final Integer id;
-	 protected final ItemType type;
+	protected static Integer id;
+	protected static String nom;
+	protected static Integer prix;
+	
+	protected Color color;
+	private int foodValue;
+	private int attackValue;
+	private int defenseValue;
+	private String appearance;
+	private Effect quaffEffect;
 	 
-// Constructor --    
-	public Item(Integer id, ItemType type, String nom, Integer prix) {
-	        
-		 this.id = id;
-	     this.type = type;
-	     this.nom = nom;
-	     this.prix = prix;
-	    
-	 }
-
-	public String getNom() {
-	        return nom;
+	private int attaqueDistanceVal;
+	private int attaqueMeleeVal;
+	private List<Sort> writtenSpells;
+	 
+	 
+//--- Constructor --    
+	public Item(Integer id,ItemType armeDistance, String nom, 
+				Integer prix, String nivDexterite) {
+		
+		 Item.id = id;
+		 nom = nom;
+	     this.color = color;
+	     Item.prix = prix;
+	     nivDexterite =nivDexterite;
+	     this.attaqueDistanceVal = 1;
+	     this.appearance = appearance == null ? nivDexterite : appearance;
+	     this.writtenSpells = new ArrayList<Sort>();
 	}
 
-	public void setNom(String nom) {
-	     this.nom = nom;
+
+	public  String details() {
+			String details = "";
+		
+		if (attackValue != 0)
+			details += "Attaque:" + attackValue;
+
+		if (attaqueDistanceVal != 1)
+			details += "Lancer :" + attaqueDistanceVal;
+		
+		if (attaqueMeleeVal > 0)
+			details += "Distance:" + attaqueMeleeVal;
+		
+		if (defenseValue != 0)
+			details += "Defence:" + defenseValue;
+
+		if (foodValue != 0)
+			details += "Aliment:" + foodValue;
+		
+		return details;
 	}
+	
+	
+	public Color color() { return color; }
+	
+	public String appearance() { return appearance; }
+	
+
+	public int foodValue() { return foodValue; }
+	public void modifyFoodValue(int amount) { foodValue += amount; }
+
+	
+	public int attackValue() { return attackValue; }
+	public void modifyAttackValue(int amount) { attackValue += amount; }
+
+
+	public int defenseValue() { return defenseValue; }
+	public void modifyDefenseValue(int amount) { defenseValue += amount; }
+		
+	public int thrownAttackValue() { return attaqueDistanceVal; }
+	public void modifyThrownAttackValue(int amount) { attaqueDistanceVal += amount; }
+
+	
+	public int rangedAttackValue() { return attaqueMeleeVal; }
+	public void modifyRangedAttackValue(int amount) { attaqueMeleeVal += amount; }
+	
+
+	public Effect quaffEffect() { return quaffEffect; }
+	public void setQuaffEffect(Effect effect) { this.quaffEffect = effect; }
+	
+	
+	public List<Sort> writtenSpells() { return writtenSpells; }
+	
+	public void addWrittenSpell(String name, int manaCost, Effect effect){
+		writtenSpells.add(new Sort(manaCost, name, manaCost, name, effect, manaCost));
+	}	
+	
+
+//--- SETTER & GETTER Nom	
+	public String getNom() { return nom; }
+
+	public void setNom(String nom) { this.nom = nom; }
   
-	public Integer getPrix() {
-	    return prix;
-	 }
 
-	public void setPrix(Integer prix) {
-	    this.prix = prix;
-	 }
+//--- SETTER & GETTER Prix
+	public Integer getPrix() { return prix; }
 
-	public ItemType getType() { return type; }
+	public void setPrix(Integer prix) { this.prix = prix; }
+
+	
+	public ItemType getType() { return getType(); }
 
 	public Integer getID() { return id; }
-	    
-	@Override
-	public abstract String toString();
-
-	public static void add(ArmeItem itemArme) {
-		// TODO Auto-generated method stub
-		
-	}
-	    
+	     
 
 }
