1f27fba510fc537750700c1ca7cde00a1cae2cfe
Chaussettecin <lachaussette@gmail.com>
2019-07-08 15:27:39 +0200

    Update ArmeDistance

--- src/Inventory/ArmesDistance.java
+++ src/Inventory/ArmesDistance.java
@@ -1,8 +1,6 @@
 package Inventory;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import java.awt.Color;
 import Inventory.ItemType;
 
 
@@ -12,27 +10,61 @@ public class ArmesDistance extends ArmeItem{
 	   
 //-- Constructor -- 
 	public ArmesDistance(Integer id, String nom, Integer prix, 
-							String descArmDist) {
+							String descArmDist, Color color) {
 	        
-		super(id, ItemType.ARME_DISTANCE, nom, prix);
+		super(id, ItemType.ARME_DISTANCE, nom, prix, nivDexterite, 
+				nivBonusAttaque);
 		
 	}
 	
-	private static Map<String,ArmesDistance> possibleArmeDist = new HashMap<String,ArmesDistance>();
-	
-	 public void initPossibleArmeDist() {
-		 possibleArmeDist.put("armDist", new ArmesDistance(0, "Lance", 20,"Petite arme en Cuire "
-	                + "Permet d'attraper ses enemis."));
+	 
+	public  Item newLance(String description){
+			Item item = new Item(1, ItemType.ARME_DISTANCE, "Lance",20, "Petite arme en Cuire " );
 			
-		 possibleArmeDist.put("armDist", new ArmesDistance (0, "Sarbacane", 50, "Objet traditionnel."
-		                + " Attaque à distance. Et on peut mettre ce que l'on veut dedans."
-		                ));
-		 
-		 possibleArmeDist.put("armDist", new ArmesDistance (0, "Boomrang", 100, "Arme fidéle"
-	                + " En bois, il assome l'ennemi et revient vers toi."
-	                + "Attention aux reflex" ));
-	 }
-
+			item.modifyAttackValue(5);
+			item.modifyThrownAttackValue(5);
+			
+			//world.addAtEmptyLocation(item, depth);
+			return item;
+	}
+	
+	
+	public 	Item newSarbacane(String descArmDist){
+			Item item = new Item(2, ItemType.ARME_DISTANCE,"Sarbacane",50, "Attaque ï¿½ distance. Et on peut mettre ce que l'on veut dedans.");
+			
+			item.modifyAttackValue(10);
+			item.modifyThrownAttackValue(3);
+			
+			//world.addAtEmptyLocation(item, depth);
+			return item;
+	}
+		
+	public Item newBoomrang(String descArmDist){
+		   Item item = new Item(3, ItemType.ARME_DISTANCE,  "Boomrang", 100," En bois, il assome l'ennemi et revient vers toi.");
+			
+		   item.modifyAttackValue(5);
+		   item.modifyDefenseValue(3);
+		   item.modifyThrownAttackValue(3);
+			
+		   //world.addAtEmptyLocation(item, depth);
+		   return item;
+		
+	}
+	
+	public Item RandomArme(){
+			
+		switch ((int)(Math.random() * 3)){
+			
+			case 0: return newLance(descArmDist);
+			case 1: return newSarbacane(descArmDist);
+			case 2: return newBoomrang(descArmDist);
+			default: return newLance(descArmDist);
+		}
+		
+	}
+	
+	
+	
 	@Override
 	 public String toString() {
 	     return id + "Nom de l'arme: " + nom + ", prix: " + prix + "Gold"
