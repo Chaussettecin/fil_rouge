31824bf45276bcf641eed3d60c74a4b0cde51852
Chaussettecin <lachaussette@gmail.com>
2019-08-16 17:07:53 +0200

    Update Humain.java

--- src/Perso/Humain.java
+++ src/Perso/Humain.java
@@ -3,17 +3,10 @@ package Perso;
 
 public abstract class Humain extends Perso{
 	
-	
 	private String description;
 
 	public Humain ( String description) {
-					
-		super (Nom, ClassesPerso, Stats, Items, Level, XP, 
-				Gold, ptv, ptvMin, minDegat, maxDegat, degat, 
-				attaque, bonusAttaque, defense, accuracy, 
-				bonusDefense, presenceArmure, mainlibre , 
-				statusEffects);
-		
+		super (nom, race);
 		// A adapter en fonction de notre Sidney
 	}
 
@@ -32,19 +25,18 @@ public abstract class Humain extends Perso{
 
 	public String descriptionSid() {
 		
-		return "Fille d’un riche propriétaire, elle est férue d’histoire et connaît le monde comme sa proche.\n" + 
-			"Elle a étudié l’ethnologies des races du disque monde. \r\n" + 
-			"Mais son passé d’adolescence lui met des battons dans les roues "
+		return "Fille d'un riche propriétaire, elle est férue d'histoire et connaît le monde comme sa proche.\n" + 
+			"Elle a étudié l'ethnologies des races du disque monde. \r\n" + 
+			"Mais son passé d'adolescence lui met des battons dans les roues "
 			+ "(les PNG ne lui feront pas confiance facilement).";
 	}
 	
 	
-	public String getDescription() {
-		return description;
-	}
+	public String getDescription() { return description; }
 
 	public void setDescription(String description) {
 		this.description = description;
 	}	
+
 
 }
