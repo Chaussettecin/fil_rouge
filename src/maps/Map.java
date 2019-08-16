72b5961a666f97d7480db4e9fa3287c375174fca
Chaussettecin <lachaussette@gmail.com>
2019-08-16 17:03:05 +0200

    Update Map.java

--- src/maps/Map.java
+++ src/maps/Map.java
@@ -1,22 +1,27 @@
-package maps;
+package Maps;
 
 import java.awt.Color;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 import asciiPanel.AsciiPanel;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
+/*
+ * Class gestion Maps
+ */
 
 public class Map {
 	
 	public char[][] map; 
-    	public int iX,jY=0;
+    public int iX,jY=0;
 	
 	AsciiPanel terminal;
 	public int hauteur;
 	public int largeur;
 	
+
+//---- CONSTRUCTOR --- 
 	public Map(AsciiPanel terminal) throws IOException {
 		
 		this.terminal=terminal;
@@ -33,18 +38,20 @@ public class Map {
 				if ((j+jY) < hauteur && (i+iX) < largeur) {
 					
 					if((j+jY)<2 || (j+jY)> hauteur-2 ||(i+iX)<2 || (i+iX)>largeur-2) {
-						terminal.write(map[j+jY][i+iX], i, j,Color.RED);
-					}
+						terminal.write(map[j+jY][i+iX], i, j,Color.GRAY);
 					
-					else {
-						
+					} else {
+					
 						if(map[j+jY][i+iX]=='D') {
 							terminal.write(map[j+jY][i+iX], i, j,Color.GRAY);
-						}else if(map[j+jY][i+iX]=='w'){
+						
+						} else if(map[j+jY][i+iX]=='w') {
 							terminal.write(map[j+jY][i+iX], i, j,Color.BLUE);
-						}else if(map[j+jY][i+iX]=='#') {
+						
+						} else if(map[j+jY][i+iX]=='#') {
 							terminal.write(map[j+jY][i+iX], i, j,Color.YELLOW);
-					    }else {
+							
+						}else {
 							terminal.write(map[j+jY][i+iX], i, j,Color.GREEN);
 						}
 					}
@@ -53,18 +60,28 @@ public class Map {
 		}
 	}
 	
+
+/*
+ * -- Premiére lecture du fichier pour déterminer la hauteur et 
+ * -- la largeur de la MAP
+ */
 	public void lireMap() throws IOException {
-		//Première lecture du fichier pour déterminer la hauteur et la largeur de la MAP
+	
 		BufferedReader br = null;
+		
 		try {
 			br = new BufferedReader(new FileReader("./src/maps/map2.txt"));
+		
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 		String line;
 		char a=' ';
 		int i=0;
+		
 		while ((line=br.readLine()) !=null) {
+			
 			largeur=line.length();
 			hauteur++;
 			
@@ -73,57 +90,72 @@ public class Map {
 		
 		//Seconde lecture pour copier le fichier dans un tableau
 		map = new char[hauteur][largeur];
+		
 		try {
 			br = new BufferedReader(new FileReader("./src/maps/map2.txt"));
+		
+		
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
-			while ((line = br.readLine()) != null) {
+		
+		
+		} while ((line = br.readLine()) != null) {
 			
+				
 				for (int j = 0 ; j< largeur ; j++) {
 				    a=line.charAt(j);
 				   	map[i][j] = a;
 				}
-				i++;
 				
-			}
-		br.close();
+		i++;
+				
 		}
+		
+		br.close();
+		System.out.println("hauteur : " + hauteur);
+		System.out.println("largeur : " + largeur);
+	}
 	
-	
+	public int getHauteur() { return hauteur; }
+
+	public void setHauteur(int hauteur) { this.hauteur = hauteur; }
+
+	public int getLargeur() { return largeur; }
+
+	public void setLargeur(int largeur) { this.largeur = largeur; }
+
+	/*
+ * Creuser dans le decor	
+ */
 	public void enleverElement(char c) {
+		
 		for (int i=0; i<hauteur; i++) {
+			
 			for (int j=0; j<largeur;j++) {
+				
 				if(map[i][j]==c) {
 					map[i][j]= ' ';
 				}
+			
 			}
+		
 		}
 	}
 	
+/*
+ * Getter & Setter - 	
+ */
+	public void setiX(int iX) { this.iX=iX; }
 	
+	public int getiX() { return this.iX; }
 	
-	//GETTER ET SETTER
-	public void setiX(int iX) {
-		this.iX=iX;
-	}
+	public int getjY() { return jY; }
 	
-	public int getiX() {
-		return this.iX;
-	}
+	public void setjY(int jY) { this.jY=jY; }
+	
 
-	
-	public int getjY() {
-		return jY;
-	}
 
-	
-	public void setjY(int jY) {
-		this.jY=jY;
-	}
-	
-	public void test() {
-		terminal.write("HELLO WORLD",10,10);
-	}
+	public void test() { terminal.write("HELLO WORLD",10,10);}
+
 	
 }
