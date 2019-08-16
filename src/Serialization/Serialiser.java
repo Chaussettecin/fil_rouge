9b62d78f7ff8a0a82376b1d9ed88aa7a7941bf39
Chaussettecin <lachaussette@gmail.com>
2019-08-16 17:15:19 +0200

    Update Serialiser.java

--- src/Serialization/Serialiser.java
+++ src/Serialization/Serialiser.java
@@ -1,72 +1,55 @@
 package Serialization;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-
-import UI.*; 
-import terminalOverflow.*;
-import maps.*;
-
-
+import Main.PlayScreen; 
 
 public class Serialiser {
 	
-
-		   GsonBuilder builder = new GsonBuilder();
-		
-		   Gson gson = builder.create();
+	GsonBuilder builder = new GsonBuilder();
+	Gson gson = builder.create();
 		   
 	
-			
-		   public Serialiser(PlayScreen screen, String fichier) {
+	public Serialiser(PlayScreen screen, String fichier) {
 		
-			Donnees toJson = new Donnees(screen); 
-			System.out.println(gson.toJson(toJson)); 
-			try {
-				BufferedWriter sav = new BufferedWriter(new FileWriter(fichier));
+	Donnees toJson = new Donnees(screen); 
+	System.out.println(gson.toJson(toJson)); 
+			
+		try {
+			
+			BufferedWriter sav = new BufferedWriter(new FileWriter(fichier));
 				
-				sav.write(gson.toJson(screen.j.position));
-				
-				sav.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			sav.write(gson.toJson(screen.persD.position));
+			sav.close();
+			
+		} catch (IOException e) {
 				e.printStackTrace();
-			}
+		}
 			
-		
-		   }
+	}
 			
-		   public Serialiser(PlayScreen screen) {
+	public Serialiser(PlayScreen screen) {
 			   
-			   Donnees toJson = new Donnees(screen); 
-			   System.out.println(gson.toJson(toJson)); 
-			   try {
-				   BufferedWriter sav = new BufferedWriter(new FileWriter("sav.json",true));
+		Donnees toJson = new Donnees(screen); 
+		System.out.println(gson.toJson(toJson)); 
+			   
+		try {
+				BufferedWriter sav = new BufferedWriter(new FileWriter("sav.json",true));
 				   
-				  //sav.write(gson.toJson(screen.j.position));
-				  sav.write(gson.toJson(toJson)+"\n");
-				   
-				   sav.close();
-			   } catch (IOException e) {
-				   // TODO Auto-generated catch block
+				sav.write(gson.toJson(toJson)+"\n");
+				sav.close();
+	
+		} catch (IOException e) {
+				 
 				   e.printStackTrace();
-			   }
-			   
-			   
-		   }
+		}
+			   		   
+	}
 		   
-		   
-			
-			
-			
-
+		 		
 }
