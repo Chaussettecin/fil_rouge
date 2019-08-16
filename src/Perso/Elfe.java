f79da9f59ca24582bed8a740509adb6d830941f3
Chaussettecin <lachaussette@gmail.com>
2019-08-16 17:08:20 +0200

    Update Elfe.java

--- src/Perso/Elfe.java
+++ src/Perso/Elfe.java
@@ -1,45 +1,43 @@
 package Perso;
 
-import Effect.statusEffect;
 
 public abstract class Elfe extends Perso {
 	
 	private String description;
 	
 
-    public Elfe(String nom, Race ClassesPerso) {
-		super(nom,ClassesPerso);
+    public Elfe(String nom, Race ClassesPerso) { 
+    	super(nom,ClassesPerso);
 		
-		//-- A faire en fonction de nos personnage 
 	}
 	
-	public Elfe(String description  ) {
-		
-		super (	Nom, ClassesPerso, Stats, Items, Level, XP, 
-				Gold, ptv, ptvMin, minDegat, maxDegat, degat, 
-				attaque, bonusAttaque, defense, accuracy, 
-				bonusDefense, presenceArmure, mainlibre , 
-				statusEffects);
-		
-		//-- A faire en fonction de nos personnage 
+	public Elfe(String description  ) { 
+		super (	nom, race); 
 	}
 
 	
 	public String descriptionHatachar() {
-		return "Un fameux personnage, il avait tout pour être heureux, médecin réputé dans le monde entier, \r\n" + 
+		return "Un fameux personnage, il avait tout pour �tre heureux, m�decin r�put� dans le monde entier, \r\n" + 
 			" une petite maison, une femme et un chien."+
-				"Courte de durée car les Elfes noirs (Cultistes) ont tué sa femme Nennae.";
+				"Courte de dur�e car les Elfes noirs (Cultistes) ont tu� sa femme Nennae.";
 	}
 	
-//-- Elle "stringuise" ttes les valeurs pr pouvoir les afficher
+
+//--- Elle "stringuise" ttes les valeurs pr pouvoir les afficher --
 	public String toString() {
 
-		return " Nom :" + this.getNom() +
-           " Point de vie :" + this.getPtv() +
-           " Level :" + this.getLevel() +
-           " Gold:" + this.getMoney() +
-           " Description:" + this.descriptionHatachar();
-		
-		
-	}	
+		return 	" Nom: " + this.getNom() +
+				" Point de vie :" + this.getPtv() +
+				" Level :" + this.getLevel() +
+				" Gold:" + this.getMoney() +
+				" Description:" + this.descriptionHatachar();
+	
+	}
+
+	public String getDescription() { return description;}
+
+	public void setDescription(String description) { this.description = description; }	
+
+
+
 }
