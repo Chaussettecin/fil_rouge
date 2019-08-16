fc10bd4d3e95bd0613ff9adca959b2329cae5e06
Chaussettecin <lachaussette@gmail.com>
2019-08-16 17:09:29 +0200

    Update Orc.java

--- src/Perso/Orc.java
+++ src/Perso/Orc.java
@@ -1,26 +1,21 @@
 package Perso;
 
 
-
 public abstract class Orc extends Perso{
 	
 	private String description;
 	
 // Fes Forelash -- 
 	
-	 public Orc(String nom, Race ClassesPerso) {
-			super(nom,ClassesPerso);
+	public Orc(String nom, Race ClassesPerso) {
+			
+		super(nom,ClassesPerso);
 			
 			//-- A faire en fonction de nos personnage 
-		}
+	}
 		
 		public Orc(String description  ) {
-			
-			super (	Nom, ClassesPerso, Stats, Items, Level, XP, 
-					Gold, ptv, ptvMin, minDegat, maxDegat, degat, 
-					attaque, bonusAttaque, defense, accuracy, 
-					bonusDefense, presenceArmure, mainlibre , 
-					statusEffects);
+			super (	nom, race);
 			
 			//-- A faire en fonction de nos personnage 
 		}
@@ -28,8 +23,10 @@ public abstract class Orc extends Perso{
 		
 
 	public String descriptionFes()  {
-		return "Ex cultiste d’Azaroth, il fut banni de Archityran (terre des ORcs) \n parce que la rationalité lui ai tombé sur  la tête"
-             + "Pour lui les Dieux n'existent plus." + "Il prefere la baston pour sauver l'humanité";
+		return "Ex cultiste d'Azaroth, il fut banni de Archityran (terre des Orcs) \n parce que la rationalité lui ai tombée sur  la tête"
+             + "Pour lui les Dieux n'existent plus." + 
+				"Il prefere la baston pour sauver l'humanité";
+		
 	}
 	
 //-- Elle "stringuise" ttes les valeurs pr pouvoir les afficher
@@ -43,9 +40,7 @@ public abstract class Orc extends Perso{
 			
 	}
 
-	public String getDescription() {
-		return description;
-	}
+	public String getDescription() { return description;}
 
 	public void setDescription(String description) {
 		this.description = description;
